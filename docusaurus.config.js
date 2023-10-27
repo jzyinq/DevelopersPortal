79453f154075cc78dfd4df5e5ab5f6524ad9bdfb
Jarosław Zywert <j.zywert@piwik.pro>
2023-10-27 08:03:31 +0200

    tune docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jzyinq.github.io/',
+  url: 'https://docu.jzy.pl/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -30,6 +30,8 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  staticDirectories: ['static'],
 
   plugins: [require.resolve('docusaurus-lunr-search')],
 
